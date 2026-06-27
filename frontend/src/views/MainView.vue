1cc3b391b8190c0fa9acd00a3027a3a73222c8fc
Soumyojyoti <soumyodeb@gmail.com>
2026-06-27 22:10:39 +0000

    changed mood names

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -11,7 +11,7 @@ const router = useRouter()
 // Constants
 const ranks = ['Iron 1', 'Iron 2', 'Iron 3', 'Bronze 1', 'Bronze 2', 'Bronze 3', 'Silver 1', 'Silver 2', 'Silver 3', 'Gold 1', 'Gold 2', 'Gold 3', 'Platinum 1', 'Platinum 2', 'Platinum 3', 'Diamond 1', 'Diamond 2', 'Diamond 3', 'Ascendant 1', 'Ascendant 2', 'Ascendant 3', 'Immortal 1', 'Immortal 2', 'Immortal 3', 'Radiant']
 const initialAgents = ['Astra', 'Breach', 'Brimstone', 'Chamber', 'Clove', 'Cypher', 'Deadlock', 'Fade', 'Gekko', 'Harbor', 'Iso', 'Jett', 'KAY/O', 'Killjoy', 'Miks', 'Neon', 'Omen', 'Phoenix', 'Raze', 'Reyna', 'Sage', 'Skye', 'Sova', 'Tejo', 'Veto', 'Viper', 'Vyse', 'Waylay', 'Yoru']
-const moods = ['serious locked in', 'chill competitive']
+const moods = ['serious', 'chill']
 const moodFilters = [
   { label: 'Any', value: 'any' },
   { label: 'Chill', value: 'chill' },
