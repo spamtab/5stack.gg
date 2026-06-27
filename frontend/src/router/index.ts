8f3fc8f7009b2408700f15700e930e32dc806819
Soumyojyoti <soumyodeb@gmail.com>
2026-06-27 14:58:18 +0000

    changed to wehhashhistory router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '../stores/authStore'
 import LandingView from '../views/LandingView.vue'
 import LoginView from '../views/LoginView.vue'
@@ -6,7 +6,7 @@ import MainView from '../views/MainView.vue'
 import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
